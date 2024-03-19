e0d9944a55d05d5efcf93b6fcde007caf9601c21
Your Name <you@example.com>
2024-03-19 16:58:04 +0100

    Iteracion3

---
+++ JAVASCRIPT/Ejercicio1/Iteracion3.js
@@ -0,0 +1,28 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+let z = 10;
+let y = 5;
+
+let t = z * y;
+console.log(t);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+let v = 10;
+let m = 2;
+
+let d = v / m;
+console.log(d);
+
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+let b = 15;
+let x = 9;
+
+let s = b / x;
+console.log(s);
+
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15,
+// teniendo dos variables p = 10 y j = 5.
+let p = 10;
+let j = 5;
+
+let o = p + j;
+console.log(o);
