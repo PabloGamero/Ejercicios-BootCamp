bf53c7f71276b09c2cd69d0ee239107165bed2bc
Your Name <you@example.com>
2024-03-19 17:29:58 +0100

    ejercicios1

--- JAVASCRIPT/Ejercicio1/Iteracion4.js
+++ JAVASCRIPT/Ejercicio1/Iteracion4.js
@@ -1,5 +1,10 @@
 // 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
-console.log(avengers[0]);
+
+let valor = avengers[0];
+console.log(valor);
 
 // 1.2 Cambia el primer elemento de avengers a "IRONMAN"
+
+avengers[0] = "IRONMAN";
+console.log(avengers[0]);
