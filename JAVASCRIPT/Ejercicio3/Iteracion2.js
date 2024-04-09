66663d955977b3f8bc690626a8492eb7f4585412
Your Name <you@example.com>
2024-04-09 10:21:31 +0200

    ejercicios

--- JAVASCRIPT/Ejercicio3/Iteracion2.js
+++ JAVASCRIPT/Ejercicio3/Iteracion2.js
@@ -2,3 +2,23 @@
 
 // Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade
 // la propiedad ***isApproved*** a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log***.
+
+const alumns = [
+  { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+  { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+  { name: "Juan Miranda", T1: false, T2: true, T3: true },
+  { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+  { name: "Raquel Benito", T1: true, T2: true, T3: true },
+];
+
+const copiaAlumno = [];
+alumns.forEach((alumno) => {
+  let acc = 0;
+  alumno.T1 == true && acc++;
+  alumno.T2 == true && acc++;
+  alumno.T3 == true && acc++;
+  if (acc >= 2) {
+    copiaAlumno.push({ ...alumno, isApproved: true });
+  } else copiaAlumno.push({ ...alumno, isApproved: false });
+});
+console.log(copiaAlumno);
---
+++ JAVASCRIPT/hubjuegos vite/src/components/index.js
@@ -0,0 +1,6 @@
+export * from "./Header/Header";
+export * from "./CardPokemons/CardPokemons";
+export * from "./Spinner/Spinner";
+export * from "./ButtonFilter/ButtonFilter";
+export * from "./Spinner/SpinnerButton";
+export * from "./CardMemory/CardMemory";
